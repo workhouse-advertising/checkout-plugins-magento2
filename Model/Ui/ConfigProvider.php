6b966ba790808fdef151d57bfb96cf2d18b97085
latitude-checkout <dheeraj.khushalani@latitudefinancial.com>
2020-11-02 18:24:17 +1100

    feat: checkout content

--- Model/Ui/ConfigProvider.php
+++ Model/Ui/ConfigProvider.php
@@ -1,18 +1,22 @@
 <?php
 namespace Latitude\Checkout\Model\Ui;
 
-use Magento\Checkout\Model\ConfigProviderInterface;
-
-/**
- * Class ConfigProvider
- */
-final class ConfigProvider implements ConfigProviderInterface
+final class ConfigProvider implements \Magento\Checkout\Model\ConfigProviderInterface
 {
     const CODE = 'latitude';
+    const SUCCESS = 0;
+    const FRAUD = 1;
+
+    protected $contentAdapter;
+
+    public function __construct(
+        \Latitude\Checkout\Model\Adapter\Content $contentAdapter
+    ) {
+        $this->contentAdapter = $contentAdapter;
+    }
 
     /**
-     * Retrieve assoc array of checkout configuration
-     *
+     * Retrieve assoc array of latitude checkout configuration
      * @return array
      */
     public function getConfig()
@@ -20,7 +24,13 @@ final class ConfigProvider implements ConfigProviderInterface
         return [
             'payment' => [
                 self::CODE => [
-                    'transactionResults' => []
+                    'transactionResults' => [
+                        self::SUCCESS => __('Success'),
+                        self::FRAUD => __('Fraud')
+                    ],
+                    'logoURL' => $this->contentAdapter->getLogoURL(),
+                    'termsURL' => $this->contentAdapter->getTermsURL(),
+                    'content' => $this->contentAdapter->getContent(),
                 ]
             ]
         ];
